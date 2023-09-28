9042ed32f748a0731dd232f919387ec7edafac2d
Marlen Ormonbaev <marlen.ormonbaev05@gmail.com>
2023-09-28 20:48:36 +0600

    added Player class

---
+++ src/Player.java
@@ -0,0 +1,21 @@
+public class Player {
+    public String name;
+    public int points = 0;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getPoints(){
+        return points;
+    }
+
+    public void setPoints(int points){
+        this.points = points;
+    }
+
+}
