7f108cb43b80d9b845f3588b6c64805a7512551b
Marlen Ormonbaev <marlen.ormonbaev05@gmail.com>
2023-09-28 21:39:21 +0600

    added method for entering player names

--- src/Master.java
+++ src/Master.java
@@ -1,4 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Master {
+
+    public static ArrayList<String> addPlayers(){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<>();
+        System.out.println("Если хотите остановить игру, введите пустую строку!!!");
+        int i = 1;
+        while(true){
+            System.out.printf("Введите имя %d игрока: ", i);
+            i ++;
+            String name = scanner.nextLine();
+            if(name.isEmpty())break;
+            names.add(name);
+        }
+        return names;
+    }
+
     public static void main(String[] args) {
+
     }
 }
