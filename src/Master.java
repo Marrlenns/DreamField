11bae1c524c548b4a0042541f9629f32ba9d50cb
Marlen Ormonbaev <marlen.ormonbaev05@gmail.com>
2023-09-28 22:28:27 +0600

    added Player constructor and update addPlayers method(made class array of players)

--- src/Master.java
+++ src/Master.java
@@ -3,22 +3,27 @@ import java.util.Scanner;
 
 public class Master {
 
-    public static ArrayList<String> addPlayers(){
+    public static Player[] addPlayers(){
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> names_list = new ArrayList<>();
         System.out.println("Если хотите остановить игру, введите пустую строку!!!");
-        int i = 1;
+        int cnt = 1;
         while(true){
-            System.out.printf("Введите имя %d игрока: ", i);
-            i ++;
+            System.out.printf("Введите имя %d игрока: ", cnt);
+            cnt ++;
             String name = scanner.nextLine();
             if(name.isEmpty())break;
-            names.add(name);
+            names_list.add(name);
+        }
+        Player[] names = new Player[names_list.size()];
+        for(int i = 0; i < names_list.size(); i++){
+            names[i] = new Player(names_list.get(i));
         }
         return names;
     }
 
     public static void main(String[] args) {
+        Player[] names = addPlayers();
 
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,10 @@ public class Player {
     public String name;
     public int points = 0;
 
+    public Player(String name){
+        this.name = name;
+    }
+
     public String getName(){
         return name;
     }
