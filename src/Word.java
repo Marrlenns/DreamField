3d2cd7babe09e143e0a71a095a90ff08951bccc3
Marlen Ormonbaev <marlen.ormonbaev05@gmail.com>
2023-09-28 20:59:55 +0600

    added Word class

---
+++ src/Word.java
@@ -0,0 +1,19 @@
+public class Word {
+    public String title, description;
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+}
